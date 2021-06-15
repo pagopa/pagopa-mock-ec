b8183d3b13828f27d1cc9797cbd0a765135cbd4f
pasqualespica <pasqualespica@gmail.com>
2021-06-15 10:26:19 +0200

    add openssl cmd to create pfx and add suffix to cert_config

--- cert/csr.sh
+++ cert/csr.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # generate private key and CSR
-openssl req -new -config cert_config -newkey rsa:2048 -nodes -keyout mockecservice.key -out mockecservice.csr
+openssl req -new -config cert_config.cfg -newkey rsa:2048 -nodes -keyout mockecservice.key -out mockecservice.csr
 
 # verify the information contained in the CSR
 openssl req -noout -text -in mockecservice.csr
+
+# create the pfx file for azure:
+openssl pkcs12 -export -out mockecservice.pfx -inkey mockecservice.key -in mockecservice.crt
