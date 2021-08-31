7ee3d9b8b1fe2347d748526ba67f8181c07fb97e
pasqualespica <pasqualespica@gmail.com>
2021-08-31 11:37:51 +0200

    add new verify error response

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -11,6 +11,9 @@ export type PAA_PAGAMENTO_IN_CORSO = t.TypeOf<typeof PAA_PAGAMENTO_IN_CORSO>;
 export const PAA_PAGAMENTO_DUPLICATO = t.literal('PAA_PAGAMENTO_DUPLICATO');
 export type PAA_PAGAMENTO_DUPLICATO = t.TypeOf<typeof PAA_PAGAMENTO_DUPLICATO>;
 
+export const PAA_SINTASSI_XSD = t.literal('PAA_SINTASSI_XSD');
+export type PAA_SINTASSI_XSD = t.TypeOf<typeof PAA_SINTASSI_XSD>;
+
 export enum POSITIONS_STATUS {
   OPEN = 1,
   IN_PROGRESS,
