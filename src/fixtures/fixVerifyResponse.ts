82269c01d9cf5e07e4a15e74ec18cc20e9f78bcf
Giovanni Berti <giovanni.berti@pagopa.it>
2024-08-02 16:51:37 +0200

    feat: add case for company name with very long name (140 chars)

--- src/fixtures/fixVerifyResponse.ts
+++ src/fixtures/fixVerifyResponse.ts
@@ -135,3 +135,26 @@ export const paVerify22 = `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlso
         </paf:paVerifyPaymentNoticeRes>
     </soapenv:Body>
 </soapenv:Envelope>`;
+
+export const paVerify23 = `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
+    xmlns:paf="http://pagopa-api.pagopa.gov.it/pa/paForNode.xsd">
+    <soapenv:Header />
+    <soapenv:Body>
+        <paf:paVerifyPaymentNoticeRes>
+            <outcome>OK</outcome>
+            <paymentList>
+            <paymentOptionDescription>
+                <amount>120.00</amount>
+                <options>EQ</options>
+                <dueDate>2030-07-31</dueDate>
+                <detailDescription>pagamentoTest</detailDescription>
+                <allCCP>true</allCCP>
+            </paymentOptionDescription>
+            </paymentList>
+            <paymentDescription>Pagamento di Test</paymentDescription>
+            <fiscalCodePA>77777777777</fiscalCodePA>
+            <companyName>Veeery long company PA name which fills all available 140 characters, (are you still reading? You should not stare at a screen for too long)</companyName>
+            <officeName>officeName</officeName>
+        </paf:paVerifyPaymentNoticeRes>
+    </soapenv:Body>
+</soapenv:Envelope>`;
