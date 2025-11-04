1a4fc7be551086718a1bfc6364d1ffaaea6476aa
Pasquale Spica <36746022+pasqualespica@users.noreply.github.com>
2025-11-04 19:30:42 +0100

    chore : developMain 2 develop (#74)
    * revert 2 main
    
    * fix src
    
    * fix: [PIDM-1127] restored changes from develop
    
    * fix: [PIDM-1127] helm chart version update
    
    * fix: [PIDM-1127] node version update
    
    * fix: [PIDM-1127] yarn.lock update
    
    * fix: [PIDM-1127] helm chart kv add
    
    * fix: [PIDM-1127] pathType: ImplementationSpecific on ingress
    
    * [PIDM-1127] fix: Resolved issue on missing keyvault reference
    
    * fix: [PIDM-1127] updated WORKLOAD_IDENTITY_CLIENT_ID env
    
    ---------
    
    Co-authored-by: Francesco Cesareo <cesareo.francesco@gmail.com>
    Co-authored-by: Matteo Raiola <matteo.raiola@nttdata.com>
    Co-authored-by: andrea-deri <andrea.derinaldis@pagopa.it>

--- src/fixtures/nodoNewMod3Responses_oldEc.ts
+++ src/fixtures/nodoNewMod3Responses_oldEc.ts
@@ -1,8 +1,8 @@
 export type MockResponse = readonly [number, string];
 
 export const paaVerificaRPTRisposta: MockResponse = [
-   200,
-   `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ws="http://ws.pagamenti.telematici.gov/"   xmlns:pag="http://www.digitpa.gov.it/schemas/2011/Pagamenti/">
+  200,
+  `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ws="http://ws.pagamenti.telematici.gov/"   xmlns:pag="http://www.digitpa.gov.it/schemas/2011/Pagamenti/">
   <soapenv:Header/>
   <soapenv:Body>
      <ws:paaVerificaRPTRisposta>
@@ -38,8 +38,8 @@ export const paaVerificaRPTRisposta: MockResponse = [
 
 
 export const paaAttivaRPTRisposta: MockResponse = [
-   200,
-   `<soapenv:Envelope
+  200,
+  `<soapenv:Envelope
    xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
    xmlns:ws="http://ws.pagamenti.telematici.gov/"
    xmlns:pag="http://www.digitpa.gov.it/schemas/2011/Pagamenti/">
